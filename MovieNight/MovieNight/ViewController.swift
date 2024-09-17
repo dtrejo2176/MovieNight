1c4b7f307f347042c38a9c3324b8d3ef37b2b2bd
David Trejo <davidarturo_trejo@hotmail.com>
2024-09-17 10:29:13 -0600

    Initial commit. Project creation

---
+++ MovieNight/MovieNight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by David Arturo Trejo Hernandez on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieNight/MovieNightTests/MovieNightTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNightTests.swift
+//  MovieNightTests
+//
+//  Created by David Arturo Trejo Hernandez on 17/09/24.
+//
+
+import Testing
+@testable import MovieNight
+
+struct MovieNightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
